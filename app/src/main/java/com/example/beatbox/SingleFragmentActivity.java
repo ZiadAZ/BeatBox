1150c185a0475a9388640ad9c56b23bdc2f994be
Ziad <ZiadZayed7@gmail.com>
2020-02-17 12:09:45 +0300

    Beat Box

---
+++ app/src/main/java/com/example/beatbox/SingleFragmentActivity.java
@@ -0,0 +1,17 @@
+package com.example.beatbox;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+    protected abstract Fragment createFragment();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beat_box);
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_con,new BeatBoxFragment()).commit();
+    }
+}
